3f8dbe2b3846dc09ac65882577fcc49c7fa780f4
tongservo1985-sketch <tongservo1985@gmail.com>
2026-01-17 11:31:14 +0700

    SWARM_SYNC: Update terraform/iam_roles.tf

---
+++ terraform/iam_roles.tf
@@ -0,0 +1,39 @@
+# IAM Role for Backend Services (Ingestion Gateway)
+resource "aws_iam_role" "backend_role" {
+  name = "${var.app_name}-backend-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Policy for generating Pre-signed URLs and reading assets
+resource "aws_iam_role_policy" "backend_s3_access" {
+  name = "S3AccessPolicy"
+  role = aws_iam_role.backend_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          aws_s3_bucket.assets.arn,
+          "${aws_s3_bucket.assets.arn}/*"
+        ]
+      }
+    ]
+  })
+}
